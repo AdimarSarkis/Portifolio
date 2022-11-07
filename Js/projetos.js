82edb894691e6886036f0407a2fc911f3614208c
Adimar Sarkis <sarkis170@gmail.com>
2022-11-07 16:13:07 -0300

    Feito a secção projetos com carousel

--- Js/projetos.js
+++ Js/projetos.js
@@ -1,25 +1,47 @@
+let contador =1;
+$("#radio3").checked = true;
 
-const info = document.querySelector(".info");
-const alturaInicial = info.clientHeight;
-const overflow = document.querySelector(".overflow");
-const btn = document.querySelector(".toggle");
+setInterval(()=>{
+    alterarImagem();
+}, 50000000000)
 
-btn.addEventListener('click', initToggle)
+function alterarImagem(){
+    contador++;
+    if(contador > 6){
+        contador = 1;
+    }
+    const radios = document.querySelectorAll("input[type='radio']")
+    radios.forEach(e => {
+        let id = e.addEventListener("click", adquirirId(e))
+    })
 
-function initToggle(e){
-    info.style.maxHeight = e.target.dataset.state === 'more'
-        ? `${info.scrollHeight}px`
-        : `${alturaInicial}px`;
-
-    e.target.setAttribute(
-        "data-state", e.target.dataset.state === "more" ? "less" : "more"
-    );
-
-    e.target.innerHTML =
-    e.target.dataset.state === "more" ? "Ver Mais" : "Ver Menos"
-
-    overflow.setAttribute(
-        "data-state",
-        e.target.dataset.state === "more" ? "visible" : "hidden"
-    )
+    function adquirirId(element){
+        let idStr = element.id;
+    }
+    
+    switch(id){
+        case 1:
+            document.getElementById("radio1").checked = true;
+            break;
+        case 2:
+            document.getElementById("radio2").checked = true;
+            break;
+        case 3:
+            document.getElementById("radio3").checked = true;
+            break;
+        case 4:
+            document.getElementById("radio4").checked = true;
+            break;
+        case 5:
+            document.getElementById("radio5").checked = true;
+            break;
+        case 6:
+            document.getElementById("radio6").checked = true;
+            break;
+        default:
+            document.getElementById("radio"+contador).checked = true;
+    }
+    
+        
+    
 }
